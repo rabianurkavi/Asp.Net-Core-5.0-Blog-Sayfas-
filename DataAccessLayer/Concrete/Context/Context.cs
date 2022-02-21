a6322fc1aa104b0d267e7e66549361bf1114af35
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-02-21 18:52:12 +0300

    .

---
+++ DataAccessLayer/Concrete/Context/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Context
+{
+    public class Context:DbContext
+    {
+        //bağlantı stringimizi tanımlayacağız. connection stringi tanımlamak içib
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-EULMR9J;database=CoreBlogDb;" +
+                "integrated security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
