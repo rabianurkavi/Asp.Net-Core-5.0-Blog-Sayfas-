c69f4f5e189bd52fafa6472aa85a603cccd75063
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-03-02 15:01:29 +0300

    CategoryList

---
+++ CoreDemo/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryList:ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
