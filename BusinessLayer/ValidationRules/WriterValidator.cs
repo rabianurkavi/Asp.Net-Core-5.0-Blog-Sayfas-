66aa6ef7996d7ac01f0b588eecf1fa960ba1b0c2
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-07-07 15:49:36 +0300

    Regex

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
@@ -14,12 +15,17 @@ namespace BusinessLayer.ValidationRules
         {
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez.");
             RuleFor(x=>x.WriterMail).NotEmpty().WithMessage("Yazar mail kısmı boş geçilemez.");
-            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez.");
-            RuleFor(x => x.WriterPassword).MinimumLength(7).WithMessage("Şifre en az 7 kaarakter olmalı");
-            RuleFor(x => x.WriterPassword).MaximumLength(12).WithMessage("Şifre en fazla 12 karakter olmalı");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Parola boş geçilemez.");
+            RuleFor(x => x.WriterPassword).MaximumLength(12).WithMessage("Parolanız en fazla 12 karakter olmalı");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişii yapın.");
             RuleFor(x => x.WriterName).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girişi yapın.");
+            RuleFor(x => x.WriterPassword).Must(IsPasswordValid).WithMessage("Parolanız en az sekiz karakter en az bir büyük harf ve bir sayı içermelidir.");
             RuleFor(x => x.WriterMail).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter girişi yapın.");
+        }
+        private bool IsPasswordValid(string arg)
+        {
+            Regex regex=new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$");
+            return regex.IsMatch(arg);
         }
     }
 }
