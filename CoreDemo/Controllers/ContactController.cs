6dc56c2b70978dd5cb82da5bc5ca256854b1d72e
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-07-09 00:57:01 +0300

    Contact Transactions

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            contactManager.ContactAdd(contact);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
