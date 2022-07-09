25249656e72752f0fe0a05126d8ce380dcd56c7d
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-07-09 17:51:36 +0300

    Authorize Procedures

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -7,10 +7,20 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
-    public class LoginController : Controller
+    [Authorize]
+    public class WriterController : Controller
     {
-        [AllowAnonymous]
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult WriterMail()
         {
             return View();
         }
