45b06f7db6328e09e7066f775f3973032ce55986
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-07-11 17:22:35 +0300

    WriterLayout, Navbar and Footer Partials

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -23,5 +23,20 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }
