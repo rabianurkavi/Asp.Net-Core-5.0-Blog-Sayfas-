27e6efb223201029c8f6fd8a9cf993f57f0d2bf6
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-07-30 16:21:38 +0300

    Message Notification

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,12 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterMessageNotification:ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "kavirabiaa@gmail.com";
+            var values = messageManager.GetInboxListWriter(p);
+            return View(values);
         }
     }
 }
