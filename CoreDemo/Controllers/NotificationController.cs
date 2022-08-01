8465ec275d8e16e636ac469b0975d7fee2f00c41
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-01 16:12:23 +0300

    Notification Last

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,17 @@ namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationDal());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var values = notificationManager.GetList();
+            return View(values);
+            
         }
     }
 }
