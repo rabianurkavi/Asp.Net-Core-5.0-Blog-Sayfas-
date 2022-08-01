0d03acc1f414206a3f6383c0c5c5746a9ec73f99
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-01 19:07:38 +0300

    Entity Framework Code First Two Foreign Keys From Same Table

--- DataAccessLayer/Concrete/Context/Context.cs
+++ DataAccessLayer/Concrete/Context/Context.cs
@@ -16,6 +16,19 @@ namespace DataAccessLayer.Concrete.Context
             optionsBuilder.UseSqlServer("server=DESKTOP-EULMR9J;database=CoreBlogDb;" +
                 "integrated security=true;");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Match>()
+                .HasOne(x => x.HomeTeam)
+                .WithMany(y => y.HomeMatches)
+                .HasForeignKey(z => z.HomeTeamID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            modelBuilder.Entity<Match>()
+                .HasOne(x => x.GuestTeam)
+                .WithMany(y => y.AwayMatches)
+                .HasForeignKey(z => z.GuestTeamID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -26,5 +39,7 @@ namespace DataAccessLayer.Concrete.Context
         public DbSet<BlogRayting> BlogRaytings { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Match> Matches { get; set; }
     }
 }
--- DataAccessLayer/Migrations/20220801160254_mig_teamtable.Designer.cs
+++ DataAccessLayer/Migrations/20220801160254_mig_teamtable.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220801160254_mig_teamtable")]
+    partial class mig_teamtable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
