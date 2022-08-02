322f43e7eae630f1c1fef5afe5f772bc31f09ee0
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-02 15:11:14 +0300

    MessageController

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Dal());
+        [AllowAnonymous]
+        public IActionResult InBox()
+        {
+            int id = 2;
+            var values = message2Manager.GetInboxListByWriter(id);
+            return View(values);
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult MessageDetails(int id)
+        {            
+            var messageValue = message2Manager.GetById(id);
+            return View(messageValue);
+        }
+    }
+}
