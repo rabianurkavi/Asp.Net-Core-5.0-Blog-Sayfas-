bf99e1115ac46e519a3606f175a6b06eb433b367
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-03 16:14:00 +0300

    Category listing for admin

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,11 @@ namespace CoreDemo.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public IActionResult Index()
         {
-            return View();
+            var values = categoryManager.GetList();
+            return View(values);
         }
     }
 }
