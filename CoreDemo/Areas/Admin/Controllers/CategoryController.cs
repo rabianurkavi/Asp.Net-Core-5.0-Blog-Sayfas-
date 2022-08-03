ef1bd2b5eddcaf2930a4edf8cc6dc8c815fe42b3
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-03 16:31:23 +0300

    PagedList

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace CoreDemo.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = categoryManager.GetList();
+            var values = categoryManager.GetList().ToPagedList(page, 5);
             return View(values);
         }
     }
--- CoreDemo/obj/Debug/net5.0/CoreDemo.MvcApplicationPartsAssemblyInfo.cs
+++ CoreDemo/obj/Debug/net5.0/CoreDemo.MvcApplicationPartsAssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("FluentValidation.AspNetCore")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("X.PagedList.Mvc.Core")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
