8d5faa4128a1d8666702677ad04be6406550bc55
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-06 17:38:39 +0300

    Statistic ViewComponents

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogDal());
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.cv=blogManager.GetList().Count();
+            ViewBag.ms = context.Contacts.Count();
+            ViewBag.cc = context.Comments.Count();
+            return View();
+        }
+    }
+}
