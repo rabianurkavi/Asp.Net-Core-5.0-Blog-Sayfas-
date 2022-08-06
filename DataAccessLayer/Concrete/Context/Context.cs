7f9cbcf7c4a63dfcaea19fd8dfc79a3f6fef487e
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-06 18:18:33 +0300

    Admin,Statistic4

--- DataAccessLayer/Concrete/Context/Context.cs
+++ DataAccessLayer/Concrete/Context/Context.cs
@@ -57,5 +57,6 @@ namespace DataAccessLayer.Concrete.Context
         public DbSet<Match> Matches { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Message2> Message2s { get; set; }
+        public DbSet<Admin> Admins { get; set; }
     }
 }
