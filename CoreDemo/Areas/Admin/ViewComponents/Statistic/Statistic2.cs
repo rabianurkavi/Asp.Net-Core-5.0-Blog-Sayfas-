a701c8e34d32cedbd088dab308b32e6bebf3d305
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-06 18:00:47 +0300

    Statistic2 Default Partial View Component

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2:ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = context.Blogs.OrderByDescending(x => x.BlogId).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            ViewBag.v2 = context.Blogs.OrderByDescending(x => x.BlogId).Select(x => x.Writer.WriterName).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
