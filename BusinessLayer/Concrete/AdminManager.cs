de9487c747034239b6c2a1df433fdac8b0d37a58
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-07 16:22:13 +0300

    Admin Transaction

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _adminDal;
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+        public Admin GetById(int id)
+        {
+            return _adminDal.Get(x => x.AdminID == id);
+        }
+
+        public List<Admin> GetList()
+        {
+            return _adminDal.GetListAll();
+        }
+
+        public void TAdd(Admin t)
+        {
+            _adminDal.Insert(t);
+        }
+
+        public void TDelete(Admin t)
+        {
+            _adminDal.Delete(t);
+        }
+
+        public void TUpdate(Admin t)
+        {
+            _adminDal.Update(t);
+        }
+    }
+}
--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic4:ViewComponent
     {
+        Context context = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.adminName = context.Admins.Where(x => x.AdminID == 1).Select(y => y.Name).FirstOrDefault(); 
+            ViewBag.adminImage = context.Admins.Where(x => x.AdminID == 1).Select(y => y.AdminImageURL).FirstOrDefault(); 
             return View();
         }
     }
