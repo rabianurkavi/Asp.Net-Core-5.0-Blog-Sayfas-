8cc9529efcdf482fdc6abbce2f802e65b8f7c81c
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-07 17:01:02 +0300

    Fetch weather via api

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,10 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.cv=blogManager.GetList().Count();
             ViewBag.ms = context.Contacts.Count();
             ViewBag.cc = context.Comments.Count();
+            string api = "6fd2dfcc6c9fce3cb2f9ff2aa57e7c56";
+            string connection= "https://api.openweathermap.org/data/2.5/weather?q=ankara&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.havaDurumu = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
