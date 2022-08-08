ec21c59515d29e850d25e6a9c0a649f0e3adf7bb
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-08 11:38:41 +0300

    Google Chart

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 20
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Spor",
+                CategoryCount = 5
+            });
+            return Json(new { jsonList = list });
+        }
+    }
+}
