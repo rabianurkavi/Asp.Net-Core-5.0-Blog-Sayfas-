e541626e47512c7ffdb7806a2ae988de420bda02
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-08 18:10:02 +0300

    Listing Data in Console with Ajax

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,47 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+        public static List<WriterModel> writers = new List<WriterModel>
+        {
+            new WriterModel
+            {
+                WriterId=1,
+                WriterName="Hüseyin"
+            },
+            new WriterModel
+            {
+                WriterId=2,
+                WriterName="Mehmet"
+            },
+            new WriterModel
+            {
+                WriterId=3,
+                WriterName="Serkan"
+            },
+            new WriterModel
+            {
+                WriterId=4,
+                WriterName="Bünyamin"
+            }
+        };
+    }
+}
---
+++ CoreDemo/Areas/Admin/Models/WriterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    public class WriterModel
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+    }
+}
