650ef838ec58ab6323e763abbe9fbce64da7f840
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-11 13:49:27 +0300

    Introduction to APIs

---
+++ BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
