9945a69591a4d91a9d4cd56f4d592420a6eb59fa
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-12 17:08:07 +0300

    communication of the project with API(update-delete)

--- CoreDemo/Controllers/EmployeeTestController.cs
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -35,6 +35,42 @@ namespace CoreDemo.Controllers
             }
             return View(class1);
         }
+        [HttpGet]
+        public async Task<IActionResult> UpdateEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44354/api/Default/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonEmployee = await responseMessage.Content.ReadAsStringAsync();//HTTP içeriğini zaman uyumsuz bir işlem olarak bir dizeye seri hale getirme.
+                var values = JsonConvert.DeserializeObject<Class1>(jsonEmployee);
+                return View(values);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmployee(Class1 class1)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(class1);
+            var content = new StringContent(jsonEmployee, Encoding.UTF8,"application/json");
+            var responseMessage = await httpClient.PutAsync("https://localhost:44354/api/Default", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(class1);
+        }
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.DeleteAsync("https://localhost:44354/api/Default/"+ id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
     public class Class1
     {
