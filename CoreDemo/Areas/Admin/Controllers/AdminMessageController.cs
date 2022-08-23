88a1dc2f0c4e64b1289d66b091bdb9245607c218
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-23 23:58:20 +0300

    admin layout edit

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Dal());
+        Context context = new Context();
+        public IActionResult InBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+            var values = message2Manager.GetInboxListByWriter(writerId);
+            return View(values);
+        }
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+            var values = message2Manager.GetSendBoxListByWriter(writerId);
+            return View(values);
+        }
+    }
+}
