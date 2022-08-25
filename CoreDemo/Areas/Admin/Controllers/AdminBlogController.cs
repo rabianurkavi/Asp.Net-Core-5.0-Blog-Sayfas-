8a061873887adc86e3af3e04f52b92e746481310
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-25 23:54:17 +0300

    Admin Blog List

---
+++ CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogDal());
+        public IActionResult Index()
+        {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
