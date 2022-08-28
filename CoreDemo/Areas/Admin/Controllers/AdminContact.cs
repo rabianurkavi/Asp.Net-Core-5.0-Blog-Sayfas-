6714248f9252d1bea17cd72d15b2498a42a69379
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-28 23:13:51 +0300

    AdminContact

---
+++ CoreDemo/Areas/Admin/Controllers/AdminContact.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminContact : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        public IActionResult Index()
+        {
+            var values = contactManager.GetList();
+            return View(values);
+        }
+    }
+}
